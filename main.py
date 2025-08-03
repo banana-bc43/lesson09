3cc9da9ff1791714d34a0462135f65ae6c88545a
吉岡祐翔 <jigangyuuto5@gmail.com>
2025-08-03 11:26:24 +0900

    こちら変更点です。

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st 
-st.title("名前記憶アプリ")
-if 'name' not in st.session_state:
-    st.session_state.name=""
+st.title("ユーザー情報入力")
+if 'user_name' not in st.session_state:
+    st.session_state.user_name=""
 name = st.text_input("あなたの名前を入力してください")
-if st.button("名前を記録"):
-    st.session_state.name=name
-st.write(f"記憶している名前:{st.session_state.name}")
+if st.button("名前を保存"):
+    st.session_state.user_name=name
+    st.success("名前を保存しました！")
+st.write(f"現在保存している名前:{st.session_state.name}")
--- pages/page1.py
+++ pages/page1.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.title("おはようございます")
+st.title("ユーザー情報ページ")
+if'user_name'in st.session_state and st.session_state.user_name:
+    st.success(f"こんにちは{st.session_state.user_name}さん!")
+    st.write("メインページで入力された名前が表示しています。")
+    st.balloons()
+else:
+    st.error("ユーザー名が設定されていません")
+    st.write("メインページで名前を入力してください。")
