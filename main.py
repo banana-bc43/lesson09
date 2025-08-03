4dd4764ba26e42c9c9dab1871bb48e034eb6ba87
吉岡祐翔 <jigangyuuto5@gmail.com>
2025-08-03 12:13:35 +0900

    これは時間かかったやつです。

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import streamlit as st
 st.title("ユーザー情報入力")
 if 'user_name' not in st.session_state:
     st.session_state.user_name=""
-name = st.text_input("あなたの名前を入力してください")
-if st.button("名前を保存"):
+if 'user_grade' not in st.session_state:
+    st.session_state.user_grade=""
+if 'user_hobbies'not in st.session_state:
+    st.session_state.user_hobbies=""   
+name = st.text_input("名前")
+grade=st.selectbox("学年",["","小学5年","小学6年","中学1年","中学2年","中学3年"])
+hobbies=st.multiselect("趣味",["読書","スポーツ","ゲーム","音楽","絵画","その他"])
+if st.button("情報を保存"):
     st.session_state.user_name=name
-    st.success("名前を保存しました！")
-st.write(f"現在保存している名前:{st.session_state.name}")
+    st.session_state.user_grade=grade
+    st.session_state.user_hobbies=hobbies
+    st.success("情報を保存しました！")
+st.write(f"現在保存している名前:{st.session_state.user_name}")
